96707945320767a5b00509f8675c69f2a95adb5a
Дмитрий К <dbk.samara@gmail.com>
2023-05-13 15:13:20 +0400

    commmit from working old schema

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
 from viberbot.api.messages.text_message import TextMessage
+import logging
+
 from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
 from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
 from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
-import time
-import logging
-import sched
-import threading
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 app = Flask(__name__)
 viber = Api(BotConfiguration(
@@ -42,44 +34,23 @@ def incoming():
         message = viber_request.message
         # lets echo back
         viber.send_messages(viber_request.sender.id, [
-            message
-        ])
-    elif isinstance(viber_request, ViberConversationStartedRequest) \
-            or isinstance(viber_request, ViberSubscribedRequest) \
-            or isinstance(viber_request, ViberUnsubscribedRequest):
-        viber.send_messages(viber_request.sender.id, [
-            TextMessage(None, None, viber_request.get_event_type())
-        ])
+               message
+            ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+                TextMessage(text="thanks for subscribing!")
+            ])
     elif isinstance(viber_request, ViberFailedRequest):
         logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
     return Response(status=200)
 
 
-def set_webhook(viber):
-    viber.set_webhook('https://volleyball78bot.onrender.com:8443/')
+viber.set_webhook('https://volleyball78bot.onrender.com:443/')
 
 
 if __name__ == "__main__":
-    scheduler = sched.scheduler(time.time, time.sleep)
-    scheduler.enter(5, 1, set_webhook, (viber,))
-    t = threading.Thread(target=scheduler.run)
-    t.start()
-
     context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=8443, debug=True, ssl_context=context)
-    app.run(port=8443)
+    app.run(host='0.0.0.0', port=443, debug=True)
 
 
-'''
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def build():
-    return 'Flask is running'
-
-if __name__ == "__main__":
-    app.run()
-'''
