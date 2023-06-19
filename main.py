1105dbb792fc7027a92898ef4b89a3575f6405d3
Дмитрий К <dbk.samara@gmail.com>
2023-06-20 00:08:03 +0400

    with logic and experiment with message return viber.request

--- main.py
+++ main.py
@@ -40,11 +40,10 @@ def incoming():
     viber_request = viber.parse_request(request.get_data().decode('utf8'))
     if isinstance(viber_request, ViberMessageRequest):
         message = viber_request.message
-        print(message)
         # прикручиваем обработку входящего мессаджа
         if message._message_type == 'text':
-            incoming_text = message
-            incoming_id = message.receiver
+            incoming_text = viber_request
+            incoming_id = viber_request.sender.id
             # output_ids, output_msg = incoming_parsing(incoming_id, incoming_text)
             # for receiver_id in output_ids:
             #     viber.send_messages(receiver_id, [
@@ -78,3 +77,19 @@ if __name__ == "__main__":
     viber.set_webhook('https://volleyball78bot.onrender.com:443/')
 
 
+
+'''
+{"event":"message",
+ "timestamp":1687200046080,
+ "chat_hostname":"SN-CALLBACK-04_",
+ "message_token":5851650804048782321,
+ "sender":{"id":"5h2COTj83ZE6IAsIcTEVGw==",
+           "name":"\xd0\x94\xd0\x9a",
+           "avatar":"https://media-direct.cdn.viber.com/download_photo?dlid=sRvVjYPcpzjFDu5Qbvqox1MFElVrX4MEnRDmf2KRRaoIIOHf4Z9lu2S1eZ8y5WNCg25FRLmTOl4g_7gv7kiRKLqjSFQ4YrG8JTbmnCo8p2_Go5fSBzHsei0hl_QRFDv5au2VFw&fltp=jpg&imsz=0000",
+           "language":"ru-RU",
+           "country":"RU",
+           "api_version":10},
+ "message":{"text":"\xf0\x9f\x99\x84",
+            "type":"text"},
+ "silent":false}
+ '''
